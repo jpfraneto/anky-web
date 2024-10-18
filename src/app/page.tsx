1cad9607499bb9ccd2ba110a59a74f5f4d031181
kithkui <kithkui@kithkuis-MacBook-Air.local>
2024-10-18 11:53:49 -0300

    aloja

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,10 @@ export default function Home() {
   return (
     <div className="w-screen h-screen bg-black text-purple-600">
       <div className="flex flex-col items-center justify-center h-full">
-        Anky
+        <p>ANKY</p>
+        <p>write 8 minutes every day</p>
+        <p>mobile app coming soon</p>
       </div>
-      <p>write 8 minutes every day</p>
     </div>
   );
 }
