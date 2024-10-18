1753f6b3a099dfaae9cb17f8479377c8475efd93
kithkui <kithkui@kithkuis-MacBook-Air.local>
2024-10-18 11:13:14 -0300

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="w-screen h-screen bg-black text-purple-600">
+      <div className="flex flex-col items-center justify-center h-full">
+        Anky
+      </div>
+      <p>write 8 minutes every day</p>
+    </div>
+  );
+}
